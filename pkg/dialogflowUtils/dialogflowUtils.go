93f2a275068906301d826874291c3f17cd788677
ccat <ccathlete01@gmail.com>
2024-04-11 10:51:48 +0300

    Progressed in building dialogflowUtils

--- pkg/dialogflowUtils/dialogflowUtils.go
+++ pkg/dialogflowUtils/dialogflowUtils.go
@@ -2,9 +2,11 @@ package dialogflowUtils
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	dialogflow "cloud.google.com/go/dialogflow/apiv2"
+	dialogflowpb "cloud.google.com/go/dialogflow/apiv2/dialogflowpb"
 	"google.golang.org/api/option"
 )
 
@@ -31,7 +33,7 @@ type NLPResponse struct {
 
 var Dfp DialogflowProcessor
 
-func (dfp *DialogflowProcessor) init(a ...string) (err error) {
+func (dfp *DialogflowProcessor) Init(a ...string) (err error) {
 	dfp.projectID = a[0]
 	dfp.authJSONFilePath = a[1]
 	dfp.lang = a[2]
@@ -47,4 +49,45 @@ func (dfp *DialogflowProcessor) init(a ...string) (err error) {
 	}
 	dfp.sessionClient = sessionClient
 	return err
+}
+
+func (dfp *DialogflowProcessor) ProcessNLP(rawMessage string, username string,
+) (r NLPResponse) {
+	sessionID := username
+	request := dialogflowpb.DetectIntentRequest{
+		Session: fmt.Sprintf("projects/%s/agent/sessions/%s", dfp.projectID,
+			sessionID),
+		QueryInput: &dialogflowpb.QueryInput{
+			Input: &dialogflowpb.QueryInput_Text{
+				Text: &dialogflowpb.TextInput{
+					Text:         rawMessage,
+					LanguageCode: dfp.lang,
+				},
+			},
+		},
+		QueryParams: &dialogflowpb.QueryParameters{
+			TimeZone: dfp.timeZone,
+		},
+	}
+	response, err := dfp.sessionClient.DetectIntent(dfp.ctx, &request)
+	if err != nil {
+		log.Fatalf("Error in communication with Dialogflow %s", err.Error())
+		return r
+	}
+	queryResult := response.GetQueryResult()
+	if queryResult.Intent != nil {
+		r.Intent = queryResult.Intent.DisplayName
+		r.Confidence = float32(queryResult.IntentDetectionConfidence)
+	}
+	r.Entities = make(map[string]string)
+	params := queryResult.Parameters.GetFields()
+	if len(params) > 0 {
+		for paramName, p := range params {
+			fmt.Printf("Param %s: %s (%s)", paramName, p.GetStringValue(),
+				p.String())
+			extractedValue := extractDialogflowEntities(p)
+			r.Entities[paramName] = extractedValue
+		}
+	}
+	return r
 }
